042f38ae728e19ace08ea6f99bf694d027e544fb
Frostdotgg <calagrimas@my.cspc.edu.ph>
2026-08-04 03:48:18 +0800

    vr route supply bldg

--- config/guidedVrRoutes.js
+++ config/guidedVrRoutes.js
@@ -213,6 +213,34 @@ const GUIDED_VR_ROUTES = Object.freeze([
       "scene-green-1st-floor-7",
     ]),
   }),
+
+  Object.freeze({
+    destination_name: "Supply and Property Building",
+    destination_node_key: "supply-property-bldg",
+    arrival_scene_key: "scene-supply-1st-floor-001",
+    scene_keys: Object.freeze([
+      "scene-guard-house",
+      "scene-general-road-10",
+      "scene-general-road-11",
+      "scene-general-road-12",
+      "scene-general-road-13",
+      "scene-general-road-14",
+      "scene-general-road-15",
+      "scene-duran-1st-floor-13",
+      "scene-general-road-19",
+      "scene-general-road-20",
+      "scene-general-road-21",
+      "scene-general-road-22",
+      "scene-general-road-23",
+      "scene-general-road-27",
+      "scene-general-road-28",
+      "scene-general-road-29",
+      "scene-general-road-30",
+      "scene-general-road-31",
+      "scene-general-road-32",
+      "scene-supply-1st-floor-001",
+    ]),
+  }),
 ]);
 
 const DEFERRED_GUIDED_VR_DESTINATIONS = Object.freeze([]);
