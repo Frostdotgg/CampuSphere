137fd6882f49a69f37b852986b4fbd0885654c3d
Frostdotgg <calagrimas@my.cspc.edu.ph>
2026-08-04 04:09:46 +0800

    vr route gym

--- config/guidedVrRoutes.js
+++ config/guidedVrRoutes.js
@@ -339,6 +339,29 @@ const GUIDED_VR_ROUTES = Object.freeze([
       "scene-audit-building-006",
     ]),
   }),
+  Object.freeze({
+    destination_name: "Gymnasium",
+    destination_node_key: "gym",
+    arrival_scene_key: "scene-gym-1st-floor-bleacher-1",
+    scene_keys: Object.freeze([
+      "scene-guard-house",
+      "scene-general-road-10",
+      "scene-general-road-11",
+      "scene-general-road-12",
+      "scene-general-road-13",
+      "scene-general-road-14",
+      "scene-general-road-15",
+      "scene-duran-1st-floor-13",
+      "scene-general-road-19",
+      "scene-general-road-20",
+      "scene-general-road-21",
+      "scene-general-road-22",
+      "scene-general-road-23",
+      "scene-general-road-24",
+      "scene-general-road-25",
+      "scene-gym-1st-floor-bleacher-1",
+    ]),
+  }),
 ]);
 
 const DEFERRED_GUIDED_VR_DESTINATIONS = Object.freeze([]);
