3da2d3225b8771ec768b75e1409b74c0173cba4e
Frostdotgg <calagrimas@my.cspc.edu.ph>
2026-08-04 04:12:10 +0800

    vr route vh

--- config/guidedVrRoutes.js
+++ config/guidedVrRoutes.js
@@ -362,6 +362,31 @@ const GUIDED_VR_ROUTES = Object.freeze([
       "scene-gym-1st-floor-bleacher-1",
     ]),
   }),
+  Object.freeze({
+    destination_name: "Villafuerte Hall",
+    destination_node_key: "villafuerte-hall",
+    arrival_scene_key: "scene-vh-1",
+    scene_keys: Object.freeze([
+      "scene-guard-house",
+      "scene-general-road-10",
+      "scene-general-road-11",
+      "scene-general-road-12",
+      "scene-general-road-13",
+      "scene-general-road-14",
+      "scene-general-road-15",
+      "scene-duran-1st-floor-13",
+      "scene-general-road-19",
+      "scene-general-road-20",
+      "scene-general-road-21",
+      "scene-general-road-22",
+      "scene-general-road-23",
+      "scene-general-road-24",
+      "scene-general-road-25",
+      "scene-pearl-park-2",
+      "scene-pearl-park-17",
+      "scene-vh-1",
+    ]),
+  }),
 ]);
 
 const DEFERRED_GUIDED_VR_DESTINATIONS = Object.freeze([]);
